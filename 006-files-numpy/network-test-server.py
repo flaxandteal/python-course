feac67386240c95ea72a1f33fb84c1c0c279675f
Phil Weir <phil.weir@numa.ie>
2016-01-17 00:11:41 +0000

    fix up the network test server

--- 006-files-numpy/network-test-server.py
+++ 006-files-numpy/network-test-server.py
@@ -9,17 +9,26 @@ Created on Sat Jan 16 19:07:16 2016
 
 # Based on the Python docs
 
-import SocketServer
+import socketserver
+import logging
 
-class EchoServerHandler(SocketServer.BaseRequestHandler):
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+
+
+class EchoServerHandler(socketserver.BaseRequestHandler):
     def handle(self):
-        message = self.request.recv(1024)
-        self.request.sendall(message)
-        
+        logger.info("New connection")
+        while True:
+            message = self.request.recv(1024)
+            if not message:
+                break
+            self.request.sendall(message)
+
 
 if __name__ == "__main__":
     HOST, PORT = "flaxandteal.co.uk", 9998
-    
-    server = SocketServer.TCPServer((HOST, PORT), EchoServerHandler)
-    
+
+    server = socketserver.TCPServer((HOST, PORT), EchoServerHandler)
+
     server.serve_forever()
